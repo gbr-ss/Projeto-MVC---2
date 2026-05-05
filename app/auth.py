c2e04edf61972a6923df1f827c783bd14da3d954
Gabriel Santos Silva <gabriel.s.silva95@aluno.senai.br>
2026-05-05 16:52:19 -0300

    dependencia do FastApi Criado

--- app/auth.py
+++ app/auth.py
@@ -44,3 +44,28 @@ def criar_token(data:dict):
 def decodificar_token(token:str):
     payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
     return payload
+
+# Dependências do FastAPI
+def get_usuario_logado(request: Request):
+
+    token = request.cookies.get("access_token")
+    
+    if not token:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Não autenticado"
+        )
+    try:
+        payload = decodificar_token(token)
+        email = payload.get("sub")
+        if email is None:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Token Invalído"
+            )
+        return payload
+    except JWTError:
+        raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Token Invalído ou expirado"
+            )
